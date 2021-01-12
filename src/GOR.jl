7c5589a13c66f495434b744d1ef571d81c1649f5
Florian Zink <florian@leppur.decode.is>
2021-01-12 16:07:47 +0000

    added src/GOR.jl

---
+++ src/GOR.jl
@@ -0,0 +1,25 @@
+module GOR
+
+using Reexport
+@reexport using Tables
+
+include("abstractgoriter.jl")
+
+include("gorfile.jl")           
+include("parquetfile.jl")
+include("utils.jl")
+
+include("verifyorder.jl")
+include("select.jl")
+include("filter.jl")
+include("mutate.jl")
+include("rename.jl")
+include("merge.jl")
+include("map.jl") 
+include("aggregate.jl")
+include("groupby.jl")
+include("join2.jl")
+
+
+end # module
+
