e69643c3d89cce20ec525bc7ad5ae095601bcbd4
Florian Zink <florian@leppur.decode.is>
2021-03-03 13:40:27 +0000

    fix mutate.jl

--- src/mutate.jl
+++ src/mutate.jl
@@ -65,5 +65,5 @@ end
 function Base.iterate(m::Mutate{I,F,C,M}, state) where {I,F,C,M}
     elt_s = state
     elt_s == nothing && return nothing
-    M(merge(elt_s[1], NamedTuple{C}(m.func(elt_s[1])))), iterate(m.rows, elt_s[2])
+    M(Base.merge(elt_s[1], NamedTuple{C}(m.func(elt_s[1])))), iterate(m.rows, elt_s[2])
 end    
